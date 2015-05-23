f5cfc37f3e37f8478ded0fa4c7f8ae86b8ace42c
Basa <mateobasaldua@gmail.com>
2015-05-23 19:30:39 -0300

    view modificada para grupos de usuarios

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,6 +1,13 @@
-#encoding:utf-8
+# encoding:utf-8
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse_lazy
 from django.db import models, connections
 
 # Create your models here.
+
+class GruposLoguin(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField(null=True)
+
+    def __unicode__(self):
+        return self.nombre
